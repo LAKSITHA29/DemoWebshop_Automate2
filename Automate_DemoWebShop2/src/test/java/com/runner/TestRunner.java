5fcf54d4f88c747809c17584e64ee01bc6019aa6
LAKSITHA29 <laksitharavi@gmail.com>
2025-04-08 10:15:08 +0530

    second commit

--- Automate_DemoWebShop2/src/test/java/com/runner/TestRunner.java
+++ Automate_DemoWebShop2/src/test/java/com/runner/TestRunner.java
@@ -3,7 +3,8 @@ package com.runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(plugin = {"pretty",
-		"json:target/cucumber-reports/CucumberHRM.json"},
+		"json:target/cucumber-reports/CucumberHRM.json",
+		 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
 
          features={"src/test/resources/com/features/DemoWebLogin.feature",
          "src/test/resources/com/features/WishList.feature",
